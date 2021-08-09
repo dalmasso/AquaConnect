3cd4dfd88d8973cbf995585b405ceec0b66639b8
loicdalmasso <loic.dalmasso@ioterop.com>
2021-08-09 17:41:20 +0200

    add config & dashboard

---
+++ Install/AquaConfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# List of AquaConnect IP Addresses
+AquaPi=192.168.1.61
+AquaTemp=192.168.1.62
+AquaCam=192.168.1.88
+AquaLighting=192.168.1.56
+AquaFeeder=192.168.1.35
+AquaDashboard=http://192.168.1.61:3000/
