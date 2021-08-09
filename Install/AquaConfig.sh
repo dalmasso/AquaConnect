09204f8223795041ae9b4843d10bf10f962f4de2
loicdalmasso <loic.dalmasso@ioterop.com>
2021-08-09 18:33:54 +0200

    update Config

--- Install/AquaConfig.sh
+++ Install/AquaConfig.sh
@@ -3,7 +3,13 @@
 # List of AquaConnect IP Addresses
 AquaPi=192.168.1.61
 AquaTemp=192.168.1.62
-AquaCam=192.168.1.88
-AquaLighting=192.168.1.56
-AquaFeeder=192.168.1.35
+AquaFeeder=192.168.1.63
+AquaEC=192.168.1.64
+AquaPH=192.168.1.65
+AquaCO2=192.168.1.66
+AquaPump=192.168.1.67
+AquaCamPlug=192.168.1.68
+AquaCam=192.168.1.69
+
 AquaDashboard=http://192.168.1.61:3000/
+AquaLighting=192.168.1.56
